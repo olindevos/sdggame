693a9e3473fa6a4d569e9adb32ac23047dd57748
olindevos <153068547+olindevos@users.noreply.github.com>
2024-11-25 16:47:31 +0100

    burger fix
    burgermenu is gefixt, dropdown displayed gewoon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,9 +13,17 @@ export default function Home() {
   // State om de richting van de animatie te bepalen
   const [direction, setDirection] = useState('right');
 
+  // State om het burger menu te tonen of te verbergen
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   // Functie om de popup te openen/sluiten
   const togglePopup = () => {
     setShowPopup(!showPopup);
+  };
+
+  // Functie om de burger menu te openen/sluiten
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   // Functie om naar de vorige POI te gaan
@@ -43,12 +51,13 @@ export default function Home() {
   return (
     <>
       <Header />
-        
+  
+      
       <div className="button-container">
-          <button onClick={togglePopup} className="open-popup-button">Display info POI1</button>
+        <button onClick={togglePopup} className="open-popup-button">Display info POI1</button>
       </div>
       <div className="play-button-container">
-           <a href="/FirstPage">Play</a>
+        <a href="/FirstPage">Play</a>
       </div>
 
       {/* Toon de popup als showPopup true is */}
@@ -62,8 +71,8 @@ export default function Home() {
                 timeout={500}
                 classNames={direction} // Gebruikt de direction ('left' of 'right') voor animatie
               >
-                <div className="poi-content"> 
-                  <div className='poi-content-title'>
+                <div className="poi-content">
+                  <div className="poi-content-title">
                     {/* Knoppen voor navigeren naar de vorige en volgende POI */}
                     <div className="navigation-buttons">
                       <button onClick={previousPOI} className="nav-button">&#8592;</button> {/* Pijl naar links */}
@@ -71,15 +80,17 @@ export default function Home() {
                       <button onClick={nextPOI} className="nav-button">&#8594;</button> {/* Pijl naar rechts */}
                     </div>
                   </div>
-                  <hr className='PCThr'></hr>
-                  <div className='popup-info'>Lorem ipsum odor amet, consectetuer adipiscing elit. Felis felis elit augue laoreet phasellus metus. Consectetur orci blandit purus himenaeos, blandit ornare nam libero. Pulvinar vehicula curae praesent urna; sollicitudin cursus sollicitudin porttitor. Pellentesque posuere sem odio montes nostra. Consectetur natoque arcu mollis natoque morbi. Apretium hac varius condimentum est vestibulum eget aliquam. Sapien natoque purus viverra interdum vivamus tempor maecenas. Vestibulum vehicula suspendisse eu orci ridiculus.Consectetur orci blandit purus himenaeos, blandit ornare nam libero.</div>
+                  <hr className="PCThr" />
+                  <div className="popup-info">
+                    Lorem ipsum odor amet, consectetuer adipiscing elit. Felis felis elit augue laoreet phasellus metus. Consectetur orci blandit purus himenaeos, blandit ornare nam libero. Pulvinar vehicula curae praesent urna; sollicitudin cursus sollicitudin porttitor. Pellentesque posuere sem odio montes nostra. Consectetur natoque arcu mollis natoque morbi. Apretium hac varius condimentum est vestibulum eget aliquam. Sapien natoque purus viverra interdum vivamus tempor maecenas. Vestibulum vehicula suspendisse eu orci ridiculus.Consectetur orci blandit purus himenaeos, blandit ornare nam libero.
+                  </div>
                 </div>
               </CSSTransition>
             </SwitchTransition>
 
             {/* Sluit de popup */}
             <div className="close-button">
-              <button onClick={togglePopup}className="close-button">X</button> {/* Attach togglePopup to close the popup */}
+              <button onClick={togglePopup} className="close-button">X</button> {/* Attach togglePopup to close the popup */}
             </div>
           </div>
         </div>
