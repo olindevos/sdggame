9f989da6c92a263a79604f7b9b566a049eaa6745
olindevos <153068547+olindevos@users.noreply.github.com>
2024-12-10 16:54:12 +0100

    .

--- src/pages/CreateJoin.jsx
+++ src/pages/CreateJoin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './CreateJoin.css';
 import InfoImage from './Info.png';
+import ReturnImage from './return.svg'
 
 export default function CreateJoin() {
   const [showPopup, setShowPopup] = useState(false); // State voor de popup
@@ -9,11 +10,16 @@ export default function CreateJoin() {
     setShowPopup(true);
     setTimeout(() => {
       setShowPopup(false); // Popup verdwijnt na 10 seconden
-    }, 10000); // 10 seconden in milliseconden
+    }, 100000); // 10 seconden in milliseconden
   };
 
   return (
     <>
+      <div className="CJI1">
+        <a className='CJI1Btn' href='Home'>
+          <img className='CJI2' src={ReturnImage} alt="" />
+        </a>
+      </div>
       <div className="CJpage">
         <div className="CJbut">
           <a className="CreateBut" href="/create">AANMAKEN</a>
