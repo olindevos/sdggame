7d060a53cfeaf47465457f6d1eccedf526f819be
olindevos <153068547+olindevos@users.noreply.github.com>
2024-12-10 15:42:10 +0100

    create pagina

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -2,7 +2,21 @@ import './create.css'
 
 export default function Create (){
     return (
-        <div className="CPage"> hallo 
+        <div className="CPage"> 
+            <div className='Cpage1'>
+                <div> 
+                     <label className="team-name">teamnaam</label>
+                     <input className='Cteam-name' ></input>
+
+                     <div className='Cpage1TC'>000-000</div>
+                </div>
+                <a className='CpagePlayBtn' href="">start spel</a>
+            </div>
+                <hr className='CpageHr'/>
+
+            <div className='Cpage2'>
+
+            </div>
         </div>
     )
 }
