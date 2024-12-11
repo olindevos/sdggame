1dc78f864059de7459ec6e78df63064c18a578a9
olindevos <153068547+olindevos@users.noreply.github.com>
2024-12-11 14:02:23 +0100

    burger menu fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,9 @@
 import './header.css';
 import { useState } from 'react';
 import burgermenu from './burgermenu.png';
+import StatsIamge from './stats.png'
+import InfoImage from './Info.png'
+import HomeImage from './home.png'
 
 
 export default function Header() {
@@ -17,9 +20,9 @@ export default function Header() {
                  <img src="/logo-gamify.png" alt="Logo" style={{ height: '60px' }} />
                 </div>
                 <div className={`InnerNav ${isOpen ? 'active' : ''}`}>
-                    <a href="/Home">Home</a>
-                    <a href="/About">About</a>
-                    <a href="/Stats">Stats</a>
+                    <a className='InnerNavBtn' href="/Home">  <img className='BurgernavImg' src={HomeImage} alt="" />Home</a>
+                    <a className='InnerNavBtn' href="/About"> <img className='BurgernavImg' src={InfoImage} alt="" />About</a>
+                    <a className='InnerNavBtn' href="/Stats"> <img className='BurgernavImg' src={StatsIamge} alt="" />Stats</a>
                 </div>
                 <div className='BurgerMenu' onClick={toggleMenu}>
                     <img src={burgermenu} alt="Burger Menu" style={{ width: '40px' }} />
